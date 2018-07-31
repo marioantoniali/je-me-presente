a38267837988fe92acbf37939301697a9cc179a7
Mario <mario.antoniali@gmail.com>
2018-07-31 13:42:27 +0200

    sdfdsgasdfsdfsd

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get '/', to: 'static_pages#home'
-  get '/contact', to: 'static_pages#contact'
-  get '/about', to: 'static_pages#about'
-  get '/about/toi', to: 'static_pages#about_toi'
-  get '/about/ton_groupe', to: 'static_pages#about_ton_groupe'
+  get '/', to: 'static_pages#home', as: 'home'
+  get '/contact', to: 'static_pages#contact', as: 'contact'
+  get '/about', to: 'static_pages#about', as: 'about'
+  get '/about/toi', to: 'static_pages#about_toi', as: 'toi'
+  get '/about/ton_groupe', to: 'static_pages#about_ton_groupe', as: 'ton_groupe'
 end
